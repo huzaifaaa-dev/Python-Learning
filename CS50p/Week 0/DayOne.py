5061ac5a4022eb11b6d072b4c416aa7e2c28abd8
Huzaifa <306003782+huzaifaaa-dev@users.noreply.github.com>
2026-07-18 10:34:58 +0500

    CS50p video 0 progress

--- CS50p/Week 0/DayOne.py
+++ CS50p/Week 0/DayOne.py
@@ -1,1 +1,10 @@
 # Here We Go!
+age = int(input("What is your age? "))
+
+print("Hello, Your age is ", end="\n")
+print(age)
+print("Next year, you will be ", end="")
+print(age + 1)
+print("You are", age, sep=" ")
+print("Hello, \"friend\"")
+print('Hello, "friend"')
