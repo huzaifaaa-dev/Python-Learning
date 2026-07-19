e64ae5f9105a31d23fd9da302b3c9e8dd0500a4b
Huzaifa <306003782+huzaifaaa-dev@users.noreply.github.com>
2026-07-19 10:42:18 +0500

    Ending the Problem Sets of Video 0

---
+++ CS50p/Week 0/einstein.py
@@ -0,0 +1,5 @@
+# Simple program to output Joules using E = mc(squared).
+mass = int(input("What is your choice of/ given mass? "))
+speed_of_light = 300000000
+mass = mass * speed_of_light
+print(f"{mass:,}")
---
+++ CS50p/Week 0/makingfaces.py
@@ -0,0 +1,3 @@
+# Replacting two different
+user_input = input("Would you like to share something? ")
+print(user_input.replace(":)","🙂").replace(":(", "🙁").title().strip())
