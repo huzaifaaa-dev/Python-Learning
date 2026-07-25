f28a69f493e5df08e4f91169c451c25da1cb8d51
Huzaifa <306003782+huzaifaaa-dev@users.noreply.github.com>
2026-07-25 22:47:55 +0500

    The Very Delayed Start to Video 1

--- CS50p/Video 1/DayOne.py
+++ CS50p/Video 1/DayOne.py
@@ -1,2 +1,24 @@
 # Day 1 — Video 1
 # Learned: ...
+x = int(input("Whats x? "))
+y = int(input("Whats y? "))
+
+if x < y:
+    print("x is less than y")
+else:  # Use elif (else if), more concise and saves space)
+    if x == y: 
+        print("x is equal to y")
+    if x > y:
+        print("x is greater than y")
+
+a = int(input("Whats a? "))
+b = int(input("Whats b? "))
+
+if a > b:
+    print("a is bigger than b")
+elif a < b:
+    print("a is smaller than b")
+elif a == b:
+    print("a is equal to b")
+
+
